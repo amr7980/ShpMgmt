d3a924552a0ea980a3cf3459b0ab82e0df93cdc3
amr7980 <amr7980@psu.edu>
2025-06-29 20:07:33 -0400

    Update Script.js

--- JS/Script.js
+++ JS/Script.js
@@ -4,22 +4,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
   form.addEventListener("submit", function (e) {
     e.preventDefault();
+
+   
     if (!form.checkValidity()) {
       form.classList.add("was-validated");
       return;
     }
 
     const shopper = {
-  email: document.getElementById("email").value.trim(),
-  name: document.getElementById("name").value.trim(),
-  phone: document.getElementById("phone").value.trim() || null,
-  age: parseInt(document.getElementById("age").value),
-  address: document.getElementById("address").value.trim(),
-  timestamp: new Date().toISOString() 
-};
-
+      id: document.getElementById("shopperId").value.trim() || null,
+      name: `${document.getElementById("firstName").value.trim()} ${document.getElementById("lastName").value.trim()}`,
+      email: document.getElementById("email").value.trim(),
+      phone: document.getElementById("phone").value.trim() || null,
+      age: parseInt(document.getElementById("age").value),
+      address: document.getElementById("address").value.trim(),
+      membership: document.getElementById("membership").value,
+      timestamp: new Date().toISOString()
+    };
 
     output.textContent = JSON.stringify(shopper, null, 2);
+
+    
     form.reset();
     form.classList.remove("was-validated");
   });
